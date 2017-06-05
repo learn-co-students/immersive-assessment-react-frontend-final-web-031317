41550b8c53c64052ab3ff6184f01eceb0e7294c2
Daniel Park <dpeace926@gmail.com>
2017-06-04 20:58:37 -0400

    Done.

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,40 @@
 import React from 'react'
 
-const Transaction = () => {
+const Transaction = (props) => {
+  var transactions = props.transactions
+  var cat = props.category
+  var newArray = []
+
+  for(var i = 0; i < transactions.length; i ++) {
+    if (cat === "All") {
+      newArray = transactions
+    } else if (transactions[i].category.slice(0, cat.length).toLowerCase() === cat.toLowerCase()) {
+      newArray.push(transactions[i])
+    }
+  }
+
+  let posted_at = newArray.map(function(transaction) {
+    return (<p>{transaction.posted_at}</p>)
+  })
+
+  let description = newArray.map(function(transaction) {
+    return (<p>{transaction.description}</p>)
+  })
+
+  let category = newArray.map(function(transaction) {
+    return (<p>{transaction.category}</p>)
+  })
+
+  let amount = newArray.map(function(transaction) {
+    return (<p>{transaction.amount}</p>)
+  })
+
   return (
     <tr>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
+      <td>{posted_at}</td>
+      <td>{description}</td>
+      <td>{category}</td>
+      <td>{amount}</td>
     </tr>
   )
 }
